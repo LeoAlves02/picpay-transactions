f06004c6649f8054b168e3c1da6a722ccf830f07
Leonardo <leoalvesbico@gmail.com>
2025-03-27 11:18:33 -0300

    feat: initial config

---
+++ src/test/java/com/leonardoalves/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoalves.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
